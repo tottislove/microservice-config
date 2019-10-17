823d95fd41165084b06164d544c1d4409a3aa8f9
totti <tottislove@163.com>
2019-10-17 15:50:01 +0800

    microservice-config-client add

---
+++ microservice-config-client/src/main/java/com/xc/ConfigClientApp.java
@@ -0,0 +1,18 @@
+package com.xc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author totti
+ * @date 2019/10/17.
+ */
+
+@SpringBootApplication
+public class ConfigClientApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApp.class, args);
+    }
+
+}
